08f0b7c4cb68673cf4e827ca5aed4b73b6988896
Albert Lu <albertlu@alum.mit.edu>
2010-10-30 09:42:52 +0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Actnow::Application.config.secret_token = '9ef8c02f61261394d34259dfd7e0415265b80be5764b854cb819154e033b13331c648cc49aa619406f7b79627252bc06f8eb6864e1d7f72c326d5e5192870384'
